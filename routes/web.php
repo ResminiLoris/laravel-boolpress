da1f5248791c9e19c50e6d29ecbaf2d9a4149e4f
ResminiLoris <resminiloris@gmail.com>
2021-10-19 19:20:47 +0200

    route_definition+admin_login

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.home');
+});
+
+Auth::routes(['register'=>false]);
+
+Route::middleware('auth')->name('admin.')->prefix('admin')->namespace('Admin')->group(function(){
+    Route::get('/', 'HomeController@index')->name('home');
 });
