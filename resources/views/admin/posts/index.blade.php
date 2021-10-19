658a41cc4c125c77c28399bded57446216b9438b
ResminiLoris <resminiloris@gmail.com>
2021-10-19 19:52:55 +0200

    created_posts_table+view

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Scritto il</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+        @forelse ($posts as $post)
+          <tr>
+            <td>{{$post->title}}</td>
+            <td>{{$post->created_at}}</td>
+            <td><a href="{{route('admin.posts.show', $post->id)}}" class="btn btn-primary"></a></td>
+          </tr>
+
+          @empty
+          <tr><td colspan="3" class="text-center">non ci sono contenuti da visualizzare</td></tr>
+        @endforelse
+        </tbody>
+      </table>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Auth::routes(['register'=>false]);
 
 Route::middleware('auth')->name('admin.')->prefix('admin')->namespace('Admin')->group(function(){
     Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
 });
