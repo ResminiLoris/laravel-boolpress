2287935dae8b24ab3c4ef11f92f499bc62367afd
ResminiLoris <resminiloris@gmail.com>
2021-10-19 20:53:46 +0200

    added_go_back_button_to_posts.show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -5,5 +5,8 @@
         <img src="{{$post->image}}" class="mb-4" alt="">
         <p class="mb-4">{{$post->content}}</p>
         <address class="mb-4">{{$post->created_at}}</address>
+        <div class="d-flex justify-content-start">
+            <a href="{{route('admin.posts.index')}}" class="btn btn-secondary">Torna alla lista</a>
+        </div>
     </div>
 @endsection
